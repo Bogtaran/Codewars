2944096d680cf6ff5dbfae52b310a969a6f6a028
Bogdan <bogtaran87@yandex.ru>
2024-09-12 20:33:59 +0600

    init

---
+++ Extract the domain name from a URL.py
@@ -0,0 +1,31 @@
+"""
+Напишите функцию, которая при задании URL в виде строки анализирует только доменное имя и возвращает его в виде
+строки.
+Например:
+
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet"
+"""
+def domain_name(url):
+    domain = ''
+    if url.count('http://') == 1:
+        url = url.replace('http://', '')
+        if url.count('www.') == 1:
+            url = url.replace('www.', '')
+            ind = url.find('.')
+        else:
+            ind = url.find('.')
+    elif url.count('https://') == 1:
+        url = url.replace('https://', '')
+        ind = url.find('.')
+    elif url.count('www.') == 1:
+        url = url.replace('www.', '')
+        ind = url.find('.')
+    elif url.count('.') >= 1:
+        ind = url.find('.')
+    domain = url[:ind]
+    return domain
+
+
+
