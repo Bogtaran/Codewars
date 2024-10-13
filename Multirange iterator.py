e476a1f43b0fa91a7824b634f46b0a8f66058e9c
Bogdan <bogtaran87@yandex.ru>
2024-10-13 20:04:23 +0600

    Полностью решил задачу. Позже необходимо будет упростить ее.

--- Multirange iterator.py
+++ Multirange iterator.py
@@ -1,56 +1,36 @@
-"""
-Давайте посмотрим на следующий генератор:
-
-def gen():
-    for i in range(2):
-        for j in range(3):
-            yield (i, j)
-
-Если мы выведем все полученные значения, то получим
-
-(0, 0)
-(0, 1)
-(0, 2)
-(1, 0)
-(1, 1)
-(1, 2)
-Для заданного списка параметров N вы должны вернуть итератор, который проходит через все возможные кортежи A, где Ai
-изменяется с 0 на Ni.
-"""
-
 def multiiter(*params):
     my_tuple = tuple(params)
-    list_iter = []
-    my_tuple = izm_params(my_tuple)
-    list_iter.append(my_tuple)
-    while my_tuple != (0,) * len(params):
-        for j in range(len(params)):
-            my_tuple = umen_params(my_tuple, j)
-            list_iter.append(my_tuple)
-
-    return print(list_iter)
-
-
-def izm_params(params):
-    my_tuple = tuple()
-    for i in range(len(params)):
-        if params[i] == 0:
-            my_tuple += (0,)
+    if len(my_tuple) == 1:
+        if my_tuple == (0,):
+            return []
         else:
-            my_tuple += (params[i] - 1,)
-    return my_tuple
+            for i in initial_list(params):
+                yield i
 
 
-def umen_params(params, j):
-    my_tuple = tuple()
-    if params[j] == 0:
-        my_tuple += (0,)
     else:
-        my_tuple += (params[j] - 1,)
-    return my_tuple
+        list_iter = initial_list(my_tuple)
+        for j in my_tuple[1:]:
+            list_iter = increase_list(list_iter, j)
 
+        for i in list_iter:
+            yield i
+
+
+def initial_list(my_tuple):
+    list_iter = []
+    for i in range(my_tuple[0]):
+        list_iter.append((i,))
+    return list_iter
+
+
+def increase_list(list_iter, j):
+    new_list_iter = []
+    for a in list_iter:
+        for i in range(j):
+            new_list_iter.append(a + (i,))
+    return sorted(new_list_iter)
 
 
 if __name__ == '__main__':
-    print((0,) * 3)
     multiiter(2, 3, 4)
