4a22309b385fb02a34313eea68dc1ff0ab5784c7
Bogdan <bogtaran87@yandex.ru>
2024-10-26 21:02:57 +0600

    init

---
+++ Rectangle into Squares.py
@@ -0,0 +1,19 @@
+'''
+https://www.codewars.com/kata/55466989aeecab5aac00003e/train/python
+'''
+
+def sq_in_rect(lng, wdth):
+    if lng == wdth:
+        return None
+    sq_list = []
+    elif lng>wdth:
+        pass
+    else:
+        pass
+
+def sq(a,b):
+
+
+
+if __name__ == '__main__':
+    main()
