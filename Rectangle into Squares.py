adb3e7d8ed63aaf46eb8bb6f5c88e5aab517c99d
Bogdan <bogtaran87@yandex.ru>
2024-10-27 14:38:02 +0600

    Окончательное решение

--- Rectangle into Squares.py
+++ Rectangle into Squares.py
@@ -2,18 +2,23 @@
 https://www.codewars.com/kata/55466989aeecab5aac00003e/train/python
 '''
 
+
 def sq_in_rect(lng, wdth):
+    sq_list = []
     if lng == wdth:
         return None
-    sq_list = []
-    elif lng>wdth:
-        pass
     else:
-        pass
+        while lng - wdth != 0:
+            if lng < wdth:
+                lng, wdth = wdth, lng
+            num = wdth
+            sq_list.append(num)
+            lng = lng - wdth
+        num = lng
+        sq_list.append(num)
 
-def sq(a,b):
-
+    return sq_list
 
 
 if __name__ == '__main__':
-    main()
+    print(sq_in_rect(20, 14))
