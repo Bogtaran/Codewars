6f60820024f3e3916121b8f21ff48d3df2d57987
Bogdan <bogtaran87@yandex.ru>
2024-11-14 16:51:06 +0600

    Изменил название файла. Стало: Digital_cypher.py. Создал директорию test.

---
+++ test/pytest_codewars.py
@@ -0,0 +1,13 @@
+import pytest
+from Digital_cypher import encode, index, sum_num_key
+
+def test_answer():
+    assert encode("scout", 1939) == [20, 12, 18, 30, 21]
+
+
+
+
+
+
+if __name__ == '__main__':
+    pytest.main()
